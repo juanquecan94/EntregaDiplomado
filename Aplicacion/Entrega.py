1aa00c74f2257e1fdb80a7c332531c03d3796f91
juanquecan94 <juandavidquecannavarro@gmail.com>
2022-07-07 21:09:46 -0500

    Se crea archivo requeriments

--- Aplicacion/Entrega.py
+++ Aplicacion/Entrega.py
@@ -1,14 +1,8 @@
 from datetime import datetime
-import numpy as np
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
 
-from PIL                      import Image
 from plotly                   import express as px
-from folium.plugins           import MarkerCluster
-from matplotlib.pyplot        import figimage
-from distutils.fancy_getopt   import OptionDummy
 
 st.set_page_config(page_title='Proyecto Final',
                     layout="wide",
